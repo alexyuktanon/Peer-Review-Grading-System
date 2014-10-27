130ffa581db93bf84c77b91f2d6971274d0562e7
kartiksaxena <kartik.s992@gmail.com>
2014-10-27 11:29:58 -0700

    edit form

--- js/instructor-dashboard.js
+++ js/instructor-dashboard.js
@@ -103,6 +103,24 @@ $(document).ready(function() {
 		$("#grading_clock_picker_"+boxId).removeClass("hide");
 		
 		
+  $("#button-edit-assignment").click(function() {
+
+    if($("#max-score-value").html()[0] != "<") {
+
+  	var score = $("#max-score-value").html();
+    $("#max-score-value").html("<input type=\"text\" id=\"edit-max-score\">");
+    $("#edit-max-score").val(score.trim());
+
+    var numGraders = $("#num-peer-graders").html();
+    $("#num-peer-graders").html("<input type=\"text\" id=\"edit-num-graders\">");
+    $("#edit-num-graders").val(numGraders.trim());
+
+    var insrtuctions = $("#instructions").html();
+    $("#instructions").html("<textarea style=\"width:500px;height: 100px\"  id=\"edit-instructions\">" + instructions + "</textarea>");
+
+
+
+		
 	});
 
 });
